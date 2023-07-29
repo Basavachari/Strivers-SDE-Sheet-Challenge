dcca0cbec6ecd8a627c86ef9c8fbb29f29fcff10
Basava Chari Boppudi <75940135+Basavachari@users.noreply.github.com>
2023-07-29 14:03:59 +0530

    Create SearchInBST.java

---
+++ SearchInBST.java
@@ -0,0 +1,27 @@
+import java.util.* ;
+import java.io.*; 
+/*
+    Following is the Binary Tree node structure:
+
+	class BinaryTreeNode<T> {
+	    public T data;
+	    public BinaryTreeNode<T> left;
+	    public BinaryTreeNode<T> right;
+
+	    BinaryTreeNode(T data) {
+	        this.data = data;
+	        this.left = null;
+	        this.right = null;
+	    }
+	}
+*/
+
+public class Solution {
+	public static Boolean searchInBST(BinaryTreeNode<Integer> root, int x) {
+		// Write your code here.
+		while(root!=null && root.data!= x){
+			root = root.data < x ? root.right : root.left;
+		}
+		return root != null;
+	}
+}
